7860759e19c3ee1238107a4470b50c2d6c118c70
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2024-03-19 03:22:16 +0300

    rayshooter

--- Assets/Scrripts/RayShooter.cs
+++ Assets/Scrripts/RayShooter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class RayShooter : MonoBehaviour {
@@ -8,6 +9,17 @@ public class RayShooter : MonoBehaviour {
     void Start()
     {
         cam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = cam.pixelWidth / 2 - size/2;
+        float posY = cam.pixelHeight / 2 - size/2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
     }
 
     void Update()
@@ -19,8 +31,18 @@ public class RayShooter : MonoBehaviour {
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit))
             {
-                Debug.Log("Hit: " + hit.point);
+                StartCoroutine(SphereIndicator(hit.point));
             }
         }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
     }
 }
